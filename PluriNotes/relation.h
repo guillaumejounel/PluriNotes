1522d88b296e6241d0e71973f6b298b94448daec
Florent Chehab <florent.chehab@etu.utc.fr>
2017-06-14 13:18:13 +0200

    best comment in relation

--- PluriNotes/relation.h
+++ PluriNotes/relation.h
@@ -64,6 +64,7 @@ public:
     //! @param t Title of the relation
     //! @param d Description of the relation
     //! @param isOriented Orientation of the relation
+    //! \return a new relation
     Relation(QString& t, QString& d, bool isOriented);
 
     //! \brief Special constructor of the class
@@ -74,6 +75,7 @@ public:
     //! @param deleted False by standard
     //! @param number Identifier of the relation, should be 0.
     //! this constructor shouldn't be used in the app (or at least once)
+    //! \return the reference relation
     Relation(QString& title, QString& description, bool isOriented, bool isReferences, bool deleted = false, unsigned int number = 0);
 
     //! \brief No argument constructor of the class
@@ -116,11 +118,13 @@ public:
     //! \brief Accessor to the oriented attribute
     bool isOriented() const {return oriented;}
 
-    //! setter for the orientation
+    //! \brief setter for the orientation
+    //! @param b new orientation
     void setOriented(bool b) {oriented = b;}
 
 
     //! \brief setter for the state of the relation
+    //! @param b newDeleteStatus
     void setDeleted(bool b) {deleted = b;}
 
     //! \brief getter for the state of the relation
@@ -131,55 +135,94 @@ public:
 
 
 
+    //! \brief is the couple (note1,note2) is inside the relation
     //! Method to know if a couple is already inside the relation
+    //! @param note1 first note of the couple
+    //! @param note2 seconde note of the couple
     bool isInside(NoteEntity* note1, NoteEntity* note2) const;
 
+    //! \brief is the note inside a couple of the relation
     //! Method to know if a note is inside
+    //! @param note the note we are looking for
     bool isInside(NoteEntity* note) const;
+    /*!
+     * \overload bool isInside(NoteEntity* note) const
+     */
 
-    //! oberload for use with a note couple directly
+
+    //! \brief is a note couple inside the relation
+    //! @param c notecouple we are looking for
     bool isInside(const NoteCouple* c) const;
+    /*!
+     * \overload bool isInside(const NoteCouple* c) const;
+     */
 
 
-    //! Method to add a note couple to the relation
+    //! \brief add a note couple in the relation
+    //! @param c notecouple we want to add
     bool addCouple(const NoteCouple* c);
 
 
-    //! Method to remove a note couple to the relation
+    //! \brief Method to remove a note couple to the relation
+    //! @param c notecouple we want to remove
     void removeCouple(const NoteCouple* c);
 
-    //! Method to remove a list of note from the relation
+    //! \breif Method to remove a list of noteCouple from the relation
+    //! @param coupleList list of notecouples we want to remove
     void removeCouple(const QList<NoteCouple*> coupleList);
+    /*!
+     * \overload removeCouple(const QList<NoteCouple*> coupleList)
+     */
 
+
+    //! \brief remove all couple with a note from the relaiton
     //! Method to remove all couples that contains a note
+    //! @param note note we want to remove
     void removeCoupleWithNote(const NoteEntity* note);
 
+    //! \brief remove all couple with one of the note from the list
+    //! Method to remove all couples that contains one of the note from the list
+    //! @param noteList list of the note we want to remove from the relation
+    void removeCoupleWithNote(const QList<NoteEntity*> noteList);
+    /*!
+     * \overload removeCoupleWithNote(const QList<NoteEntity*> noteList)
+     */
 
-    //! Method to remove all couples with predecessor :     //only for oriented !
+    //! \brief remove Couples with a specific predecesso
+    //! Method to remove all couples with predecessor : only for oriented !
+    //! @param note note we want to remove
     void removeCoupleWithNotePredecessor(const NoteEntity* note);
 
-    //! Method to remove all couples that contains one of the note from the list
-    void removeCoupleWithNote(const QList<NoteEntity*> noteList);
 
 
-    //! Method to get all the predecessors of a note in a relation
+
+
+    //! \brief Method to get all the predecessors of a note in a relation
     //! A QSet is used to make sure we don't have twice the same note...
+    //! @param note note we want to remove
     QSet<NoteEntity*> successorsOf(const NoteEntity* note) const;
 
-    //! Method to get all the predecessors of a note in a relation
+    //! \brief Method to get all the predecessors of a note in a relation
     //! A QSet is used to make sure we don't have twice the same note...
     //! For obbious reason we want to be able not to take into account predecessors that are already in the trash or archived
+    //! @param note the note we want to find the predecessor of
+    //! @param outOfArchives optional parameter if we want to look outside archives and deleted stuff
     QSet<NoteEntity*> predecessorsOf(const NoteEntity* note, bool outOfArchives = false) const;
 
-    //! A method to know if a note has Predecessors in the relation
+    //! \brief A method to know if a note has Predecessors in the relation
     //! For the same reason as for "predecessorsOf" we my want not to look into the trash and archives
+    //! @param note the note we want to check if it has predecessor
+    //! @param outOfArchives optional parameter if we want to look outside archives and deleted stuff
     bool hasPredecessors(const NoteEntity *note, bool outOfArchives = false) const;
 
 
 
+    //! \brief Save relation to XML
     //! Method to save the relation and its content in the xml file
+    //! @param stream the resulting stream
     void saveToXML(QXmlStreamWriter& stream) const;
 
+    //! \brief load from xml
     //! Method to load the relations from the XML file
     static Relation* loadFromXML(QXmlStreamReader& stream);
 };
